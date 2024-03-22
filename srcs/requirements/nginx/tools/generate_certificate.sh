4e715336637499407619f23974a7b9ccd54a473c
Chahbi Youssef <ychahbi1337@outlook.com>
2024-03-22 17:00:46 +0000

    push

---
+++ srcs/requirements/nginx/tools/generate_certificate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -newkey rsa:4096 -keyout /etc/ssl/private/privkey.pem -out /etc/ssl/certs/fullchain.pem -config /etc/ssl/sslconf -days 365
+
+cp /etc/ssl/certs/fullchain.pem /etc/nginx/certs/fullchain.pem
+cp /etc/ssl/private/privkey.pem /etc/nginx/certs/privkey.pem
