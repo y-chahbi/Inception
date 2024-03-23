a45bcf3fca19ec429ad8d247631728cdbd58cc9c
Chahbi Youssef <ychahbi1337@outlook.com>
2024-03-23 06:25:44 +0000

    installed nginx and cgi webserv now can serv php files

--- srcs/requirements/nginx/tools/generate_certificate.sh
+++ srcs/requirements/nginx/tools/generate_certificate.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -newkey rsa:4096 -keyout /etc/ssl/private/privkey.pem -out /etc/ssl/certs/fullchain.pem -config /etc/ssl/sslconf -days 365
+openssl req -x509 -nodes -newkey rsa:4096 -keyout /etc/ssl/private/privkey.pem -out /etc/ssl/certs/fullchain.pem -config /etc/ssl/sslconf -days 365 > /dev/null 2>&1
 
 cp /etc/ssl/certs/fullchain.pem /etc/nginx/certs/fullchain.pem
 cp /etc/ssl/private/privkey.pem /etc/nginx/certs/privkey.pem
+
+echo 'fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+include fastcgi_params;' > /etc/nginx/snippets/fastcgi-php.conf
+
+sed -i 'listen = /run/php/php8.2-fpm.sock' /etc/php/8.2/fpm/pool.d/www.conf > /dev/null 2>&1
+
+
+service php8.2-fpm start
+
+nginx -g "daemon off;"
