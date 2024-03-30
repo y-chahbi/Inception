f0d8c65a34bfaef60c27b6a61e783ac599735f01
Chahbi Youssef <ychahbi1337@outlook.com>
2024-03-30 05:50:40 +0000

    soon i will finsh ftp server

--- Makefile
+++ Makefile
@@ -3,13 +3,11 @@ go:
 	mkdir -p /var/Data/MD
 	mkdir -p /var/Data/WP
 	mkdir -p /var/Data/RDS
+
 end:
 	cd srcs && docker-compose down --rmi all --volumes --remove-orphans && cd ..
-clear: $(end)
-	docker system prune -a
-	sudo rm -rf /var/Data/MD
-	sudo rm -rf /var/Data/WP
-	sudo rm -rf /var/Data/RDS
 
-re: $(end) 
-	$(go)
+clear: end
+	docker system prune -a
+
+re: end go
--- srcs/requirements/bonus/vsftpd/tools/script.sh
+++ srcs/requirements/bonus/vsftpd/tools/script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-vsftpd -opasv_min_port=30000 -opasv_max_port=30009
+useradd $FTPUSER
 
-
+echo $FTPUSER:$FTPPASS | chpasswd
+apt-get install ftp -y
+# Add FTP user to vsftpd userlist
+echo $FTPUSER >> /etc/vsftpd.userlist
+echo $FTPUSER >> /etc/shells
+cp /etc/vsftpd.userlist /etc/vsftpd.user_list
+mkdir -p /var/run/vsftpd/empty
+chown -R $FTPUSER:$FTPUSER /srv/ftp
+sed -i '/^root/s/^/#/' /etc/ftpusers
+# service vsftpd reload
+# Start vsftpd in the background
+vsftpd
