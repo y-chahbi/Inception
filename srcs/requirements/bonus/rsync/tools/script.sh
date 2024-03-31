192eec1394f248f7c2c6eb0409605585bc8a36fc
Chahbi Youssef <ychahbi1337@outlook.com>
2024-03-31 11:54:25 +0000

    push

--- srcs/requirements/bonus/rsync/tools/script.sh
+++ srcs/requirements/bonus/rsync/tools/script.sh
@@ -2,5 +2,5 @@
 
 export TIMESTAMP=$(date +"%Y-%m-%d");
 
-mysqldump -h $IP_MARIADB -P $PORT_MARIADB -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DBNAME > /var/backups/databases/database.sql
 rsync -avz --exclude='/wp-content/cache/' /var/wordpress /var/backups/$TIMESTAMP
+# mysqldump -h $IP_MARIADB -P $PORT_MARIADB -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DBNAME > /var/backups/databases/database.sql
