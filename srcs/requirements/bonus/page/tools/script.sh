d053c1987174c76470550cf9cad4d3081f30198e
Chahbi Youssef <ychahbi1337@outlook.com>
2024-05-22 19:56:47 +0100

    push

--- srcs/requirements/bonus/page/tools/script.sh
+++ srcs/requirements/bonus/page/tools/script.sh
@@ -2,7 +2,7 @@
 
 wget https://www.free-css.com/assets/files/free-css-templates/download/page291/dozecafe.zip
 
-mkdir /var/t_tmp
+mkdir -p /var/t_tmp
 unzip dozecafe.zip -d /var/t_tmp
 
 cp -r /var/t_tmp/html/* /var/www/html
